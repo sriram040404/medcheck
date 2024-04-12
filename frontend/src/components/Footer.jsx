038a122bc07320a4577cf426112aff5b0de36104
SPACECENTRE <akkilalagar05@gmail.com>
2024-04-12 17:07:26 +0530

    footer fixed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,21 +3,20 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='footer'>
-        <footer>
-    <div class="foot">
-        <div class="head">
-                MEDCHECK
-        </div>
-        <div class="cont">
-            Let's stay connected
-            Follow us on instagram: medcheck_28
-            <div  class="cont1">
-                Terms of service | Privacy policy | Accesibility policy
+      <footer>
+        <div class="foot">
+            <div class="head">
+                    MEDCHECK
+            </div>
+            <div class="cont">
+                Let's stay connected
+                Follow us on instagram: medcheck_28
+                <span>
+                    Terms of service | Privacy policy | Accesibility policy
+                </span>
             </div>
         </div>
-
-    </div>
-    </footer>
+      </footer>
     </div>
   )
 }
