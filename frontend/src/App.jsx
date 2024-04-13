15fe56fe2811673bfba749f10ff3a77f1a3ac8c2
sriram040404 <145564929+sriram040404@users.noreply.github.com>
2024-04-13 16:51:39 +0530

    updated med page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import NavBar from './components/NavBar'
 import HomePage from './components/HomePage'
 import CreateReminder from './components/CreateReminder'
 import ViewReminders  from './components/ViewReminders'
-import About from './components/About'
+import Medicine from './components/Medicine'
 import Footer from './components/Footer'
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/create" element={<CreateReminder />} />
       <Route path="/view" element={<ViewReminders />} />
-      <Route path="/about" element={<About />} />
+      <Route path="/medicine" element={<Medicine />} />
     </Routes>
     <Footer />
     </>
