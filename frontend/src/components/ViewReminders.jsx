2172e6ee7de3dc43e1264c8d3626ed964fbca811
SPACECENTRE <akkilalagar05@gmail.com>
2024-04-13 16:59:04 +0530

    view reminders

--- frontend/src/components/ViewReminders.jsx
+++ frontend/src/components/ViewReminders.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const ViewReminders = () => {
   return (
     <div>
       <div className='view-reminder'>
       <h1>All Reminders</h1>
+      <h2></h2>
       </div>
     </div>
   )
