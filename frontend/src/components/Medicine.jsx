9dc7779e478acb350d1955e0b934b3d695e6f56b
SPACECENTRE <akkilalagar05@gmail.com>
2024-04-13 17:01:06 +0530

    errors fixed

--- frontend/src/components/Medicine.jsx
+++ frontend/src/components/Medicine.jsx
@@ -4,13 +4,13 @@ const Medicine = () => {
   return (
     <div class="med-cont">
         <ul>
-            <li><b>HYDROCODON:</b> Used for Severe pain and as a cough suppresant for adults[Every 12hrs].</li><br>
-            <li><b>METFORMIN:</b> Used for type 2 diabetes and to treat PCOS[max 2000mg per day].</li><br>
-            <li><b>LOSARTAN:</b> Used for high blood pressure and reduses risk of stroke[depending on doctor's prescription].</li><br>
-            <li><b>ANTIBIOTICS:</b> Used to cure bacterial infection[depending on the doctor's prescription].</li><br>
-            <li><b>SNTIHISTAMINES:</b> Used to treat symptoms of most Allergies[take every day with a fixed schedule].</li><br>
-            <li><b>OMEPRAZOLE:</b> Used to reduce acid in Stomach and treat indigestion heartburn[once a day before eating].</li><br>
-            <li><b>LEVOTHYROXIN:</b> Used for underactive thyroid gland[take 30mins before breakfast].</li><br>
+            <li><b>HYDROCODON:</b> Used for Severe pain and as a cough suppresant for adults[Every 12hrs].</li><br />
+            <li><b>METFORMIN:</b> Used for type 2 diabetes and to treat PCOS[max 2000mg per day].</li><br />
+            <li><b>LOSARTAN:</b> Used for high blood pressure and reduses risk of stroke[depending on doctor's prescription].</li><br />
+            <li><b>ANTIBIOTICS:</b> Used to cure bacterial infection[depending on the doctor's prescription].</li><br />
+            <li><b>SNTIHISTAMINES:</b> Used to treat symptoms of most Allergies[take every day with a fixed schedule].</li><br />
+            <li><b>OMEPRAZOLE:</b> Used to reduce acid in Stomach and treat indigestion heartburn[once a day before eating].</li><br />
+            <li><b>LEVOTHYROXIN:</b> Used for underactive thyroid gland[take 30mins before breakfast].</li><br />
             <li><b>ATORVASTATIN:</b> Used for treating high cholesterol prevent heart disease[take once a day].</li>
         </ul>
     </div>
